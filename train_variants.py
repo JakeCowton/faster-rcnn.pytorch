f4d22ae20f7629f62cdb03446fd9a8e854a26f2d
Jake Cowton <j.cowton2@ncl.ac.uk>
2018-10-15 17:22:50 +0100

    Better log paths for models

--- train_variants.py
+++ train_variants.py
@@ -15,8 +15,8 @@ class TrainVariants(object):
 
     def generate_session(self, i):
         dt = datetime.now().strftime("%y%m%d")
-        dt = f"{dt}{i}"
-        return dt
+        session = f"{dt}{i}"
+        return session
 
     def inet_pascal_pigs(self):
         session = self.generate_session(1)
@@ -29,6 +29,7 @@ class TrainVariants(object):
                                   "cuda": True,
                                   "mGPUs": self.mGPUs,
                                   "session": session,
+                                  "log_path": f"logs/pascal_voc_{session}",
                                  })
         trained_pascal.train()
 
@@ -36,7 +37,7 @@ class TrainVariants(object):
         trained_pigs = Trainer({"dataset": "pigs_voc",
                                 "net": "res101",
                                 "epochs": self.epochs * 2,
-                                "disp_interval": 100,
+                                "disp_interval": 10,
                                 "bs": self.bs,
                                 "nw": self.nw,
                                 "cuda": True,
@@ -49,37 +50,40 @@ class TrainVariants(object):
                                 "resume_data": "pascal_voc",
                                 "transfer": True,
                                 "resume_classes": 21,
+                                "log_path": f"logs/pascal_pigs_voc_{session}",
                                })
         trained_pigs.train()
 
     def inet_pigs(self):
         session = self.generate_session(2)
         logging.info(f"[{session}] Training on Pigs VOC")
-        trained_pascal = Trainer({"dataset": "pigs_voc",
+        trained_pigs = Trainer({"dataset": "pigs_voc",
                                   "net": "res101",
                                   "epochs": self.epochs,
-                                  "disp_interval": 100,
+                                  "disp_interval": 10,
                                   "nw": self.nw,
                                   "cuda": True,
                                   "mGPUs": self.mGPUs,
                                   "session": session,
+                                  "log_path": f"logs/pigs_voc_{session}",
                                  })
-        # trained_pascal.train()
+        trained_pigs.train()
 
     def inet_pascal(self):
-        session = self.generate_session(2)
+        session = self.generate_session(3)
         logging.info(f"[{session}] Training on Pigs VOC")
-        trained_pascal = Trainer({"dataset": "pascal_voc_agnostic",
-                                  "net": "res101",
-                                  "epochs": self.epochs,
-                                  "disp_interval": 100,
-                                  "nw": self.nw,
-                                  "cuda": True,
-                                  "mGPUs": self.mGPUs,
-                                  "session": session,
-                                  "cag": True,
-                                 })
-        # trained_pascal.train()
+        trained_pascal_agnostic = Trainer({"dataset": "pascal_voc_agnostic",
+                                           "net": "res101",
+                                           "epochs": self.epochs,
+                                           "disp_interval": 100,
+                                           "nw": self.nw,
+                                           "cuda": True,
+                                           "mGPUs": self.mGPUs,
+                                           "session": session,
+                                           "cag": True,
+                                           "log_path": f"logs/pascal_agnostic_{session}",
+                                          })
+        trained_pascal_agnostic.train()
 
     def run(self):
         """
